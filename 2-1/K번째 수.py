eebf53dc0dcbb75e4797f4484305a498977d5a29
dangdang2222 <ksy02031@gmail.com>
2022-04-29 17:59:10 +0900

    error code

---
+++ 2-1/K번째 수.py
@@ -0,0 +1,12 @@
+case_num = int(input())
+result = []
+for i in range(case_num):
+    N, s,e,k = map(int, input().split(' '))
+    a= []
+    a= list(map(int, input().split()))
+    b = a[s-1:e]
+    b.sort()
+    result.append(b[k-1])
+
+for i in range(case_num):
+    print('#',(i+1),' ',result[i])
