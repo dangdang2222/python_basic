4672a63e95973140dabc555b8aa8683e34a13e57
dangdang2222 <ksy02031@gmail.com>
2022-05-02 21:57:26 +0900

    final commit

--- 2-1/정다면체.py
+++ 2-1/정다면체.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin=open("in5.txt","rt")
+#sys.stdin=open("in5.txt","rt")
 
 N,M=map(int,input().split())
 
@@ -18,3 +18,22 @@ maximum = max(count)
 for index, counts in enumerate(count):
     if counts==maximum:
         print(index+1, end=' ')
+
+'''
+import sys
+sys.stdin = input("input.txt","rt")
+n,m= map(int,input().split)
+cnt = [0]*(n+m)
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        cnt[i+j]+=1
+max=-2147000000
+for i in range(n+m+1):
+    if cnt[i]>max:
+        max=cnt[i]
+
+for i in range(n+m+1):
+    if cnt[i]==max:
+        print(i, end=' ')
+'''
