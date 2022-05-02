4cfc632f6caed6022a2f4dc9abb9a71de373d3c3
dangdang2222 <ksy02031@gmail.com>
2022-05-02 20:45:22 +0900

    final commit
    print 형식 주의

--- 2-1/K번째 수.py
+++ 2-1/K번째 수.py
@@ -1,5 +1,4 @@
 import sys
-#sys.stdin=open("in5.txt","rt")
 
 case_num = int(input())
 result = []
@@ -13,3 +12,15 @@ for i in range(case_num):
 
 for i in range(case_num):
     print('#',(i+1),' ',result[i])
+
+'''
+import sys
+sys.stdin = open("input.txt","rt")
+T=int(input())
+for t in range(T):
+    n, s,e,k = map(int,input().split())
+    a = list(map(int,input().split()))
+    a=a[s-1:e]
+    a.sort()
+    print("#%d %d" %(t+1,a[k-1]))
+'''
