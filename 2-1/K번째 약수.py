0b50f082c75db850622aac1b69d4fa67a40cdec3
dangdang2222 <ksy02031@gmail.com>
2022-05-02 20:37:28 +0900

    final commit
    for else 구문 주의

--- 2-1/K번째 약수.py
+++ 2-1/K번째 약수.py
@@ -8,3 +8,20 @@ if len(a)<K:
     print(-1)
 else:   
     print(a[K-1])
+
+
+'''
+import sys
+sys.stdin=open("input.txt","rt")
+
+n,k = map(int,input().split())
+cnt=0
+for i in range(1,n+1):
+    if n%1==0:
+        cnt+=1
+    if cnt==k:
+        print(i)
+        break
+else:
+    print(-1)
+'''
